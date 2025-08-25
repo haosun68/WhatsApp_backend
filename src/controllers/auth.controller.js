5e6ae66d0cf13418b82f12c1ad8fbcdd75aa0a7a
haosun68 <haosun8686@gmail.com>
2025-08-24 19:39:15 -0500

    validation with register

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,10 +1,20 @@
+import { createUser } from "../services/auth.service.js";
+
 export const register = async (req, res, next) => {
-    try {
-        res.send(req.body);
-    } catch (error) {
-      next(error);
-    }
-  };
+  try {
+    const { name, email, picture, status, password } = req.body;
+    const newUser = await createUser({
+      name,
+      email,
+      picture,
+      status,
+      password,
+    });
+    res.json(newUser);
+  } catch (error) {
+    next(error);
+  }
+};
 
 export const login = async (req, res, next) => {
   try {
