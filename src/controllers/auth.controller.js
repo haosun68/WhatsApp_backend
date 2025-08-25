45663e2e4ab201b75f7ca4c9b98ad6236bbac5cc
haosun68 <haosun8686@gmail.com>
2025-08-24 21:28:06 -0500

    Implement login/logout, refresh token, and auth middleware

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,7 @@
-import { createUser } from "../services/auth.service.js";
-import { generateToken } from "../utils/generateToken.js";
+import { createUser, signUser } from "../services/auth.service.js";
+import { findUser } from "../services/user.service.js";
+import { generateToken, verifyToken } from "../services/token.service.js";
+import createHttpError from "http-errors";
 
 export const register = async (req, res, next) => {
   try {
@@ -44,6 +46,18 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res, next) => {
   try {
+    const { email, password } = req.body;
+    const user = await signUser(email, password);
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const refresh_token = await generateToken(
+      { userId: user._id },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
   } catch (error) {
     next(error);
   }
@@ -51,6 +65,7 @@ export const login = async (req, res, next) => {
 
 export const logout = async (req, res, next) => {
   try {
+    res.json({ message: "logged out !" });
   } catch (error) {
     next(error);
   }
@@ -58,6 +73,28 @@ export const logout = async (req, res, next) => {
 
 export const refreshToken = async (req, res, next) => {
   try {
+    const refresh_token = req.cookies.refreshtoken;
+    if (!refresh_token) throw createHttpError.Unauthorized("Please login.");
+    const check = await verifyToken(
+      refresh_token,
+      process.env.REFRESH_TOKEN_SECRET
+    );
+    const user = await findUser(check.userId);
+    const access_token = await generateToken(
+      { userId: user._id },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    res.json({
+      access_token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        picture: user.picture,
+        status: user.status,
+      },
+    });
   } catch (error) {
     next(error);
   }
